1e9982e0d70e2f5ee41bfa1c8942bf0498dbbfb9
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-01-27 21:08:09 -0800

    Define Inngest function to create new user in database if not exists

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,10 +1,39 @@
 import { inngest } from "./client";
+import { db } from "@/configs/db";
+import { USER_TABLE } from "@/configs/schema";
+import { eq } from "drizzle-orm";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const CreateNewUser = inngest.createFunction(
+  { id: "create-user", retries: 1 },
+  { event: "user.create" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const { user } = event.data;
+    // Get Event Data
+    const result = await step.run(
+      "Checking if the user exists in the database...",
+      async () => {
+        // Check Is User Already Exist
+        const result = await db
+          .select()
+          .from(USER_TABLE)
+          .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+
+        if (result?.length == 0) {
+          //If Not, Then add to DB
+          const userResp = await db
+            .insert(USER_TABLE)
+            .values({
+              name: user?.fullName,
+              email: user?.primaryEmailAddress?.emailAddress,
+            })
+            .returning({ USER_TABLE });
+          return userResp;
+        }
+        return result;
+      }
+    );
+    return result?.length === 0
+      ? "New user successfully created."
+      : "User already exists in the database.";
   }
 );
