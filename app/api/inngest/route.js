6dfe6cc687f56ce21a6975b34c5a8184b502737f
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-01-27 21:04:12 -0800

    put in explaination for the inngest routes file

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,19 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { helloWorld } from "@/inngest/functions";
-export const runtime = 'edge';
-// Create an API that serves zero functions
+import { CreateNewUser } from "@/inngest/functions";
+export const runtime = "edge";
+{
+  /*This file sets up an API route (/api/inngest/route.js) that allows handling HTTP GET, POST, and PUT requests. 
+  It connects the incoming requests to the CreateNewUser serverless function, which checks if a user exists in 
+  the database and adds them if not. It enables the functionality of your provider by processing the request 
+  triggered by the front-end.*/
+}
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  streaming:'allow',
+  streaming: "allow",
   functions: [
     /* your functions will be passed here later! */
-    helloWorld,
+    CreateNewUser,
   ],
 });
