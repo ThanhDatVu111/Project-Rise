82e02e4e66855492159a9c5bee755f247079b5f9
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-01-27 21:06:39 -0800

    Add user login check and API call create new user if not exists

--- app/provider.js
+++ app/provider.js
@@ -1,8 +1,6 @@
 "use client";
-import { db } from "@/configs/db";
-import { USER_TABLE } from "@/configs/schema";
 import { useUser } from "@clerk/nextjs";
-import { eq } from "drizzle-orm";
+import axios from "axios";
 import React, { useEffect } from "react";
 
 function Provider({ children }) {
@@ -16,34 +14,18 @@ function Provider({ children }) {
   }, [user]);
   //If the user becomes available (i.e., the user logs in), the effect will run, triggering the CheckIsNewUser() function.
 
+  {
+    /*High-Level Flow Overview
+    Frontend (provider.js): Checks if the user is logged in and triggers a POST request to an API endpoint.
+    API Endpoint (/api/create-user/route.js): Receives the user data from the frontend, fires an event (user.create) using Inngest.
+    Inngest Function (CreateNewUser): Listens for the user.create event, checks the database for the user, and adds them to the database if they don’t exist.
+    */
+  }
   const CheckIsNewUser = async () => {
-    console.log("Checking if the user exists in the database...");
-
-    // Check if the user already exists in the database
-    const result = await db
-      .select()
-      .from(USER_TABLE)
-      .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
-
-    console.log("Database query result:", result);
-
-    if (result?.length === 0) {
-      console.log("User not found in the database. Adding new user...");
-      // If user is not found, insert them into the database
-      const userResp = await db
-        .insert(USER_TABLE)
-        .values({
-          name: user?.fullName,
-          email: user?.primaryEmailAddress?.emailAddress,
-        })
-        .returning({ id: USER_TABLE.id });
-
-      console.log("New user added with ID:", userResp?.id);
-    } else {
-      console.log("User already exists in the database.");
-    }
+    const resp = await axios.post("/api/create-user", { user: user }); //is a method used to send data to a server via an HTTP POST request.
+    console.log(resp.data);
   };
-  //The await ensures that the function doesn't proceed until the data is fetched from the database.*/
+
   return <>{children}</>; //This will render whatever child components (like pages) are passed to the Provider component.
 }
 
