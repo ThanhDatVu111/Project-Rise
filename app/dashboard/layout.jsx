14b34812b22d0bd98b98ca38dada7f19cdf8e3f1
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-02-08 18:48:17 -0800

    add page and layout for /dashboard

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+"use client";
+import SideBar from "./_components/SideBar";
+import DashboardHeader from "./_components/DashboardHeader";
+
+function DashboardLayout({ children }) {
+  return (
+      <div>
+        <div className="md:w-64 hidden md:block fixed">
+          <SideBar />
+        </div>
+        <div className="md:ml-64">
+          <DashboardHeader />
+          <div className="p-10">{children}</div>
+        </div>
+      </div>
+  );
+}
+
+export default DashboardLayout;
