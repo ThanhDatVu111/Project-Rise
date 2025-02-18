dd0352935111997524093e3907e24540b28ff2a0
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-02-17 21:25:46 -0800

    debug and make small update

--- app/dashboard/_components/CourseCardItem.jsx
+++ app/dashboard/_components/CourseCardItem.jsx
@@ -3,20 +3,22 @@ import { RefreshCw } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { format } from "date-fns";
 
 function CourseCardItem({ course }) {
+  const currentDate = format(new Date(), "dd MMM yyyy"); // "16 Feb 2025"
   return (
     <div className="border rounded-lg shadow-lg p-5">
       <div>
         <div className="flex justify-between items-center">
           <Image src={"/knowledge.png"} alt="other" width={50} height={50} />
           <h2 className="text-[10px] p-1 px-2 rounded-full bg-primary text-white">
-            16 Feb 2025
+            {currentDate}
           </h2>
         </div>
 
         <h2 className="mt-3 font-medium text-lg">
-          {course?.courseLayout?.courseTitle}
+          {course?.courseLayout?.courseTitle || course?.courseLayout?.course_title}
         </h2>
         <p className="text-sm line-clamp-2 text-gray-500 mt-2">
           {course?.courseLayout?.summary}
