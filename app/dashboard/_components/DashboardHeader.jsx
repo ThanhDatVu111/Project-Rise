e0f1e126c8ffb13f85f2e2673b3273c35330ece1
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-02-28 16:40:34 -0800

    fixed header dashboard

--- app/dashboard/_components/DashboardHeader.jsx
+++ app/dashboard/_components/DashboardHeader.jsx
@@ -1,35 +1,37 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 function DashboardHeader() {
-  const path=usePathname();
+  const path = usePathname();
   return (
-    <div className={`p-5 shadow-md flex ${path=='/dashboard'?'justify-end':'justify-between'}`}>
-       {path!='/dashboard'&& 
-        <Link href={'/dashboard'}>
-           <div className='flex gap-2 items-center'>
-            
-             <Image src={'/logo.svg'} alt='logo' width={30} height={30}/>
-            
-                <h2 className="font-bold text-xl">Easy Study</h2>
+    <div
+      className={`p-5 shadow-md flex ${
+        path == "/dashboard" ? "justify-end" : "justify-between"
+      }`}
+    >
+      {/* Logo appears only when not in dashboard and go back to dashboard if user click on to the logo*/}
+      {path != "/dashboard" && (
+        <Link href={"/dashboard"}>
+          <div className="flex items-center">
+            <Image src={"/Riselogo.svg"} alt="logo" width={170} height={100} />
           </div>
-          </Link>  }
+        </Link>
+      )}
 
-        {/* Dashboard Button added */}
-          <div className='flex items-center gap-3'>
-          <UserButton/>
-          <Link href={'/dashboard'}>
-          <Button>Dashboard</Button> 
-
-          </Link>
-          </div>
+      {/* Dashboard Button added */}
+      <div className="flex items-center gap-3">
+        <UserButton />
+        <Link href={"/dashboard"}>
+          <Button>Dashboard</Button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
