e5742b9f44053637269a9cf8bda604a3b69f467a
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-02-28 16:41:52 -0800

    put comment into explain how children work

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,18 +2,20 @@
 import SideBar from "./_components/SideBar";
 import DashboardHeader from "./_components/DashboardHeader";
 
-{/*This file wraps the entire route and contains UI elements that remain the same across different pages in that route (e.g., navbar, sidebar, footer).*/}
+{
+  /*This file wraps the entire route and contains UI elements that remain the same across different pages in that route (e.g., navbar, sidebar, footer).*/
+}
 function DashboardLayout({ children }) {
   return (
-      <div>
-        <div className="md:w-64 hidden md:block fixed">
-          <SideBar />
-        </div>
-        <div className="md:ml-64">
-          <DashboardHeader />
-          <div className="p-10">{children}</div>
-        </div>
+    <div>
+      <div className="md:w-64 hidden md:block fixed">
+        <SideBar />
       </div>
+      <div className="md:ml-64">
+        <DashboardHeader />
+        <div className="p-10">{children}</div>
+      </div>
+    </div>
   );
 }
 
