a045057e749ccfe6220bde32d802cea1b581f5c6
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-03-04 21:48:06 -0800

    add footer

--- app/course/layout.jsx
+++ app/course/layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DashboardHeader from "../dashboard/_components/DashboardHeader";
+import Footer from "../dashboard/_components/Footer";
 
 function CourseViewLayout({ children }) {
   return (
     <div>
       <DashboardHeader />
       <div className="mx-10 md:mx-36 lg:px-44 mt-10">{children}</div>
+      <Footer />
     </div>
   );
 }
