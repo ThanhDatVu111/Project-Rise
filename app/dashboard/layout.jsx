e80c74c0f0ebd5bba0681469b75d1ac102d632f6
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-03-14 17:10:21 -0700

    add CourseCountContext and comment to explain the feat

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,21 +1,32 @@
 "use client";
+import React, { useState } from "react";
 import SideBar from "./_components/SideBar";
 import DashboardHeader from "./_components/DashboardHeader";
-
+import { CourseCountContext } from "../_context/CourseCountContext";
 {
-  /*This file wraps the entire route and contains UI elements that remain the same across different pages in that route (e.g., navbar, sidebar, footer).*/
+  /*this wrap the entire dashboard*/
 }
 function DashboardLayout({ children }) {
+  const [totalCourse, setTotalCourse] = useState(0);
+  {
+    /* By wrapping your component tree in the CourseCountContext.Provider, 
+    every child component within that layout can access the same context value. 
+    This means that if one component updates totalCourse using 
+    setTotalCourse (update in getCourseList inside CourseList), 
+    all other components that consume the context will receive the updated value.*/
+  }
   return (
-    <div>
-      <div className="md:w-64 hidden md:block fixed">
-        <SideBar />
+    <CourseCountContext.Provider value={{ totalCourse, setTotalCourse }}>
+      <div>
+        <div className="md:w-64 hidden md:block fixed">
+          <SideBar />
+        </div>
+        <div className="md:ml-64">
+          <DashboardHeader />
+          <div className="p-10">{children}</div>
+        </div>
       </div>
-      <div className="md:ml-64">
-        <DashboardHeader />
-        <div className="p-10">{children}</div>
-      </div>
-    </div>
+    </CourseCountContext.Provider>
   );
 }
 
