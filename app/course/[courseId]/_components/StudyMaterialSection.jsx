1569ffbeb725f7a278e57f892ad71cd2b8ca2920
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-03-14 17:11:15 -0700

    take out Q&A part, dont want to do it anymore

--- app/course/[courseId]/_components/StudyMaterialSection.jsx
+++ app/course/[courseId]/_components/StudyMaterialSection.jsx
@@ -24,14 +24,7 @@ function StudyMaterialSection({ courseId, course }) {
       icon: "/quiz.png",
       path: "/quiz",
       type: "quiz",
-    },
-    {
-      name: "Question/Answer",
-      desc: "Help to pratice your learning",
-      icon: "/qa.png",
-      path: "/qa",
-      type: "qa",
-    },
+    }
   ];
 
   useEffect(() => {
@@ -51,7 +44,7 @@ function StudyMaterialSection({ courseId, course }) {
   return (
     <div className="mt-5">
       <h2 className="font-medium text-xl">Study Material</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-5 mt-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-3">
         {MaterialList.map((item, index) => (
           <MaterialCardItem
             item={item}
